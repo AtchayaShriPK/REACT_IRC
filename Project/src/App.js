a7cabed53070e8bc6d288eec1c0db52c571ead03
AtchayaShriPK <153495616+AtchayaShriPK@users.noreply.github.com>
2023-12-14 12:37:37 +0530

    Add files via upload

---
+++ Project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/LoginSignUp/loginsign';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
